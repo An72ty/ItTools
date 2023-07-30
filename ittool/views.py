067bdbbca06808de6a2d9001eede8a0da17cacac
An72ty <An72ty@gmail.com>
2023-07-30 19:17:56 +0500

    changed css

--- ittool/views.py
+++ ittool/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from .models import Plugin
 
 
+menu = ['ITTOOLS', 'Download', 'Plugins', 'About']
+
+
 def index(request):
     return render(request, 'ittool/index.html')
 
 
 def plugins(request):
     plugins = Plugin.objects.all()
-    
+
     context = {'plugins': plugins}
     return render(request, 'ittool/plugins.html', context)
