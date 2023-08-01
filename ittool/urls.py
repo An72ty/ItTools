b86286a2441efa92325af2cde00bb4f18163d3ee
An72ty <An72ty@gmail.com>
2023-08-01 12:08:27 +0500

    .

--- ittool/urls.py
+++ ittool/urls.py
@@ -5,5 +5,8 @@ from .views import index, plugins
 app_name = 'ittool'
 urlpatterns = [
     path('', index, name='index'),
-    path('plugins/', plugins, name='plugins')
+    path('download/', index, name='download'),
+    path('plugins/', plugins, name='plugins'),
+    path('eternal-arts/', index, name='eternal-arts'),
+    path('about/', index, name='about'),
 ]
--- ittool/views.py
+++ ittool/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 from .models import Plugin
 
 
-menu = ['ITTOOLS', 'Download', 'Plugins', 'About']
+menu = [{'title': 'Download', 'url': 'ittool:download'},
+        {'title': 'Plugins', 'url': 'ittool:plugins', },
+        {'title': 'Eternal Arts', 'url': 'ittool:eternal-arts', },
+        {'title': 'About', 'url': 'ittool:about'}
+        ]
 
 
 def index(request):
-    return render(request, 'ittool/index.html')
+    context = {'menu': menu, 'page_selected': -1}
+    return render(request, 'ittool/index.html', context)
 
 
 def plugins(request):
     plugins = Plugin.objects.all()
 
-    context = {'plugins': plugins}
+    context = {'menu': menu, 'page_selected': 1, 'plugins': plugins}
     return render(request, 'ittool/plugins.html', context)
