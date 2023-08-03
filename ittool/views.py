bf34d818a9d6dda372e2cbb0766322fd0f0fe630
An72ty <An72ty@gmail.com>
2023-08-03 15:15:24 +0500

    created top-panel

--- ittool/views.py
+++ ittool/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import Plugin
 
 
-menu = [{'title': 'Download', 'url': 'ittool:download'},
-        {'title': 'Plugins', 'url': 'ittool:plugins', },
-        {'title': 'Eternal Arts', 'url': 'ittool:eternal-arts', },
-        {'title': 'About', 'url': 'ittool:about'}
+menu = [{'title': 'Download', 'url': 'ittool:download', 'page_number': 0},
+        {'title': 'Plugins', 'url': 'ittool:plugins', 'page_number': 1},
+        {'title': 'Eternal Arts', 'url': 'ittool:eternal-arts', 'page_number': 2},
+        {'title': 'About', 'url': 'ittool:about', 'page_number': 3}
         ]
 
 
