5d29e3a8314a73b48ee6da6d242074f48d3d13dd
An72ty <An72ty@gmail.com>
2023-08-08 13:17:32 +0500

    fixed sorting bug

--- ittool/views.py
+++ ittool/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models.manager import BaseManager
 from .models import Plugin
 
 
@@ -7,6 +8,54 @@ menu = [{'title': 'Downloads', 'url': 'ittool:downloads', 'page_number': 0},
         {'title': 'Eternal Arts', 'url': 'ittool:eternal-arts', 'page_number': 2},
         {'title': 'About', 'url': 'ittool:about', 'page_number': 3}
         ]
+
+
+def list_to_queryset(model, data):
+    from django.db.models.base import ModelBase
+
+    if not isinstance(model, ModelBase):
+        raise ValueError(
+            "%s must be Model" % model
+        )
+    if not isinstance(data, list):
+        raise ValueError(
+            "%s must be List Object" % data
+        )
+
+    pk_list = [obj.pk for obj in data]
+    return model.objects.filter(pk__in=pk_list)
+
+
+def str_queryset_to_list(model, data) -> list:
+    from django.db.models.base import ModelBase
+
+    if not isinstance(model, ModelBase):
+        raise ValueError(
+            "%s must be Model" % model
+        )
+    if not isinstance(data, str):
+        raise ValueError(
+            "%s must be Str Object" % data
+        )
+
+    raw_list = data.split('[')
+    raw_element = raw_list[1]
+    raw_list_2 = raw_element.split(']')
+    raw_element_2 = raw_list_2[0]
+    raw_list_3 = raw_element_2.split(', ')
+
+    model_list = []
+    for model_str in raw_list_3:
+        el = model_str.split(' ')
+        try:
+            e = el[1]
+        except IndexError:
+            return []
+        el2 = e.split('>')
+        model_name = el2[0]
+        model_list.append(model.objects.get(name=model_name))
+
+    return model_list
 
 
 def index(request):
@@ -20,18 +69,21 @@ def plugins(request):
         plugins_filter = request.GET.get('plugin_or_dev_name')
         name_filter = request.GET.get('name_filter')
         date_filter = request.GET.get('date_filter')
+        plugins_list = []
         if plugins_filter:
-            plugins = []
             for plugin in Plugin.objects.all():
                 if plugins_filter.lower() in plugin.name.lower():
-                    plugins.append(plugin)
+                    plugins_list.append(plugin)
                 elif plugins_filter.lower() in plugin.dev_name.lower():
-                    plugins.append(plugin)
-            plugins.sort()
-        if name_filter:
-            plugins = Plugin.objects.order_by(name_filter)
-        if date_filter:
-            plugins = Plugin.objects.order_by(date_filter)
+                    plugins_list.append(plugin)
+            plugins = list_to_queryset(Plugin, plugins_list)
+        if request.GET.get('past_plugins'):
+            past_plugins = list_to_queryset(
+                Plugin, str_queryset_to_list(Plugin, request.GET.get('past_plugins')))
+            if name_filter:
+                plugins = past_plugins.order_by(name_filter)
+            if date_filter:
+                plugins = past_plugins.order_by(date_filter)
 
     context = {'menu': menu, 'page_selected': 1, 'plugins': plugins}
     return render(request, 'ittool/plugins.html', context)
